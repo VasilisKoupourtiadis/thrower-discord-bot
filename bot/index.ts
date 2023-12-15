d23efbe7a0c31a7cb73aa0b92f82a867fcee2830
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-15 10:31:01 +0100

    Register discord client

---
+++ bot/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { dependencies } from "./dependencies";
+
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.login(dependencies.discordToken);
