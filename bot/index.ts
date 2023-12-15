800e872922ca070ff66e17deb6754e198b0d5379
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-15 14:57:24 +0100

    Configure interactionCreate listener to work with "leaderboard" command

--- bot/index.ts
+++ bot/index.ts
@@ -7,13 +7,15 @@ import {
   ActivityType,
   TextChannel,
   User,
+  EmbedBuilder,
 } from "discord.js";
 
 const enum CommandNamesAndOptions {
   Throwing = "throwing",
-  Check = "check",
   ThrowingOptionName = "person-throwing",
+  Check = "check",
   CheckOptionName = "person-to-be-checked",
+  Leaderboard = "leaderboard",
 }
 
 const client = new Client({
@@ -58,9 +60,13 @@ client.on("interactionCreate", async (interaction) => {
           await channel.send({
             content: botReply,
           });
+          await interaction.reply({
+            content: "Command successfully registered ✔",
+            ephemeral: true,
+          });
+        } else {
+          await interaction.reply(botReply);
         }
-
-        await interaction.reply(botReply);
       }
       break;
     case CommandNamesAndOptions.Check:
@@ -75,9 +81,47 @@ client.on("interactionCreate", async (interaction) => {
           await channel.send({
             content: botReply,
           });
-        }
 
-        await interaction.reply(botReply);
+          await interaction.reply({
+            content: "Command successfully registered ✔",
+            ephemeral: true,
+          });
+        } else {
+          await interaction.reply(botReply);
+        }
+      }
+      break;
+    case CommandNamesAndOptions.Leaderboard:
+      {
+        const embed = new EmbedBuilder()
+          .setColor("Random")
+          .setTitle("Leaderboard")
+          .setAuthor({
+            name: "Thrower",
+            iconURL: "https://i.imgur.com/RoCxZ3b.png",
+          })
+          .setDescription("Here are Consultopia's worst throwers")
+          .setThumbnail("https://i.imgur.com/wer9sUY.gif")
+          .addFields(
+            { name: "1. Daco", value: "5k throws" },
+            { name: "2. Hugo", value: "4k throws" },
+            { name: "3. Sam", value: "2k throws" }
+          )
+          .setImage("https://i.imgur.com/KHNxBqg.gif")
+          .setTimestamp();
+
+        if (!correctChannel) {
+          await channel.send({
+            embeds: [embed],
+          });
+
+          await interaction.reply({
+            content: "Command successfully registered ✔",
+            ephemeral: true,
+          });
+        } else {
+          await interaction.reply({ embeds: [embed] });
+        }
       }
       break;
     default:
