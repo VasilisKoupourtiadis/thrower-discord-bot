8a94ded1223c3f60cbd61e075131b8593fa354a4
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-15 10:36:45 +0100

    Add listenter function(s) for ready and interactionCreate event

--- bot/index.ts
+++ bot/index.ts
@@ -1,7 +1,12 @@
 import "dotenv/config";
 import { dependencies } from "./dependencies";
 
-import { Client, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  ActivityType,
+  TextChannel,
+} from "discord.js";
 
 const client = new Client({
   intents: [
@@ -10,6 +15,42 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.DirectMessages,
   ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is online`);
+
+  client.user?.setActivity({
+    name: "Daco throw",
+    type: ActivityType.Watching,
+  });
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const channel = client.channels.cache.get(
+    dependencies.channelId
+  ) as TextChannel;
+
+  const correctChannel = interaction.channel?.id === channel?.id;
+
+  const user = interaction.options.get("person-throwing")?.user;
+
+  const botReply = `<@${user?.id}> IS THROWING. THEY'RE TRASH`;
+
+  if (interaction.commandName === "throwing" && correctChannel) {
+    interaction.reply(botReply);
+  } else if (interaction.commandName === "throwing" && !correctChannel) {
+    channel.send({
+      content: botReply,
+    });
+
+    interaction.reply({
+      content: "Commmand successfully registered",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(dependencies.discordToken);
