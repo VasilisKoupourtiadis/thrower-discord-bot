51363a23015c25f77110b34f0eb39f33d454b182
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-20 12:11:58 +0100

    Add new option to slash command "throwing"

--- bot/commands/command-collection.ts
+++ bot/commands/command-collection.ts
@@ -7,6 +7,22 @@ export const commands: Command[] = [
     description: "Imagine throwing 🤣",
     options: [
       {
+        name: "counter-to-update",
+        description: "The throw counter to update (raid or normal)",
+        type: ApplicationCommandOptionType.String,
+        choices: [
+          {
+            name: "throw-count",
+            value: "normal",
+          },
+          {
+            name: "raid-throw-count",
+            value: "raid",
+          },
+        ],
+        required: true,
+      },
+      {
         name: "person-throwing",
         description: "The person who's throwing (it's Daco isn't it?)",
         type: ApplicationCommandOptionType.User,
