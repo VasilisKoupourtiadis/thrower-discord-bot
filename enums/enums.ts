ec5acb35936e184982af90445b4ed6dcd2c52245
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2023-12-20 12:09:57 +0100

    Export enums

---
+++ enums/enums.ts
@@ -0,0 +1,15 @@
+export const enum CommandNamesAndOptions {
+  Throwing = "throwing",
+  ThrowingOptionName = "person-throwing",
+  CounterToUpdate = "counter-to-update",
+  ThrowCount = "normal",
+  RaidThrowCount = "raid",
+  Check = "check",
+  CheckOptionName = "person-to-be-checked",
+  Leaderboard = "leaderboard",
+}
+
+export const enum Channels {
+  Bot = "bot",
+  Throw = "throw",
+}
